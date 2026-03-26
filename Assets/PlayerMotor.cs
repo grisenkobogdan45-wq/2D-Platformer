b99ed86be92bf76b28902f1b8cd9e7f9ea2055c9
Gawron10001 <kacper.predkiewicz@gmail.com>
2026-03-26 09:43:16 +0100

    Advanced player movement

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -7,17 +7,46 @@ public class PlayerMotor : MonoBehaviour
     Vector2 direction;
     public float jumpForce = 5;
     public float speed = 5;
-    private Rigidbody2D rb;
+    public float maxSpeed = 10;
+    public float stoppingForce = 5;
+    private Rigidbody2D rigidbody2D;
     private bool _canJump = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+        MovePlayer();
+        HandleMaxSpeed();
+        PlayerStopping();
+    }
+
+    private void MovePlayer()
+    {
+        rigidbody2D.AddForce(new Vector2(direction.x * speed, 0));
+    }
+
+    private void HandleMaxSpeed()
+    {
+        if (rigidbody2D.linearVelocityX >= maxSpeed)
+        {
+            rigidbody2D.linearVelocityX = maxSpeed;
+        }
+        else if (rigidbody2D.linearVelocityX <= -maxSpeed)
+        {
+            rigidbody2D.linearVelocityX = -maxSpeed;
+        }
+    }
+
+    private void PlayerStopping()
+    {
+        if (direction.x == 0 && rigidbody2D.linearVelocityX != 0)
+        {
+            rigidbody2D.AddForce(new Vector2(-rigidbody2D.linearVelocityX * stoppingForce, 0));
+        }
     }
 
     private void OnMove(InputValue value)
@@ -29,7 +58,7 @@ public class PlayerMotor : MonoBehaviour
     {
         if (_canJump)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             _canJump = false;
         }
     }
