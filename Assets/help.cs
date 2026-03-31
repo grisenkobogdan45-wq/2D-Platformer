e5b8417f37c90f8015e8f5aff8e529e7a10121ac
grisenkobogdan45-wq <grisenkobogdan45@gmail.com>
2026-03-31 09:06:07 +0200

    hp2

--- Assets/help.cs
+++ Assets/help.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class help : MonoBehaviour
 {
+    public float Max_Health = 100;
+    private float Health;
+    private bool Can_DMG = true;
+    public float InvincibilityTimer = 2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Health = Max_Health;
     }
 
     // Update is called once per frame
-    void Update()
+    public void AddDamage(float damage)
     {
-        
+        Health -= damage;
+        Debug.Log(damage);
     }
 }
