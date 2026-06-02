68898cb137f95c9b856358a9aaa634f3061c66d0
grisenkobogdan45-wq <grisenkobogdan45@gmail.com>
2026-06-03 01:50:50 +0200

    move platform

---
+++ Assets/Mov_Platform.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mov_Platform : MonoBehaviour
+{
+	public Transform platform;
+	public Transform startPoint;
+	public Transform endPoint;
+	public float speed = 1.5f;
+
+	int direction = 1;
+
+	private void Update()
+	{
+		Vector2 target = currentMovementTarget();
+
+		platform.position = Vector2.Lerp(platform.position,target,speed*Time.deltaTime);
+
+		float distance = (target - (Vector2)platform.position).magnitude;
+
+		if(distance <= 0.1f)
+		{
+			direction *= -1;
+		}
+	}
+
+	Vector2 currentMovementTarget()
+	{
+		if (direction == 1)
+		{
+			return startPoint.position;
+		}
+		else
+		{
+			return endPoint.position;
+		}
+	}
+
+
+	private void OnDrawGizmos()
+	{
+		if(platform!=null && startPoint!=null && endPoint!=null)
+		{
+			Gizmos.DrawLine(platform.transform.position, startPoint.position);
+			Gizmos.DrawLine(platform.transform.position, endPoint.position);
+		}
+	}
+
+}
